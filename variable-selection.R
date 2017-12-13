bd8b37359343acaf5fa72d1ae164d33ea66ec427
mrlee23 <mrlee_23@naver.com>
2017-12-13 18:29:12 +0900

    Update variable-selection.R

--- variable-selection.R
+++ variable-selection.R
@@ -1,7 +1,10 @@
-## install.packages('glmnet')
-library(glmnet)
-library(leaps)
 source('./load.R')
+## install.packages('glmnet')
+## install.packages('bestglm')
+library(leaps)
+library(glmnet)
+library(bestglm)
+library(boot)
 plot.gen <- function (regfit, output.file) {
     regfit.summary = summary(regfit)
     png(output.file)
@@ -30,33 +33,101 @@ coef.print <- function (regfit) {
     print(which.max(regfit.summary$adjr2))
     print(coef(regfit,which.max(regfit.summary$adjr2)))
 }
-subset.simple <- function (data, method) {
-     ## regfit.full <- regsubsets(BANKRUPTCY ~ ., nvmax=dim(data)[2], data=data)
-     ## regfit.fwd <- regsubsets(BANKRUPTCY ~ ., nvmax=dim(data)[2], data=data, method="forward")
-     ## regfit.bwd <- regsubsets(BANKRUPTCY ~ ., nvmax=dim(data)[2], data=data, method="backward")
-    ## return(c(regfit.full, regfit.fwd, regfit.bwd))
-    if (method == 'full') {
-            return(regsubsets(BANKRUPTCY ~ ., nvmax=dim(data)[2], data=data))
-        } else {
-            return(regsubsets(BANKRUPTCY ~ ., nvmax=dim(data)[2], data=data, method=method))
-            }
+subset.simple <- function (data, method=NULL) {
+    if (is.null(method)) {
+        return(regsubsets(BANKRUPTCY ~ ., nvmax=dim(data)[2], data=data))
+    } else {
+        return(regsubsets(BANKRUPTCY ~ ., nvmax=dim(data)[2], data=data, method=method))
+    }
 }
-?regsubsets
-dim(data)[2]
-regfit.full <- regsubsets(BANKRUPTCY ~ . , nvmax=31, data=data)
-regfit.fwd <- regsubsets(BANKRUPTCY ~ . , data=data, method="forward")
-regfit.bwd <- regsubsets(BANKRUPTCY ~ . , data=data, method="backward")
-summary(regfit.full)
-plot.gen(regfit.full, './plots/subset.png')
+#' Modified for logistic. These codes are from https://github.com/yufree/democode
+#' Currently not use this function.
+predict.regsubsets <- function(coef, newdata) {
+    form  <-  as.formula(~.)
+    mat  <-  model.matrix(form, newdata)
+    xvars  <-  names(coef)
+    res = mat[, xvars] %*% coef
+    res = exp(res)/(1+exp(res))
+    return(res)
+}
+clear.names <- function (names) {
+    if (names[1] == "(Intercept)") {
+        names = names[-1]
+    }
+    return(unique(gsub("[a-z]*", "", names[-1])))
+}
+gen.formula <- function (coef, y) {
+    as.formula(paste(y, "~", paste(clear.names(names(coef)), collapse="+")))
+}
+error.glm <- function (formula, train.data, test.data, thresh) {
+    glm.fit = glm(formula, data=train.data, family="binomial")
+    glm.res = predict(glm.fit, data=test.data, type="response")
+    if (any(glm.res < 0 | glm.res > 1)) stop ("로지스틱 회귀로 예측한 값이 0 또는 1이 아닙니다.")
+    glm.pred = rep("NO", length(test.data[,1]))
+    glm.pred[glm.res[glm.res>thresh]] = "YES"
+    return(mean(glm.pred != test.data$BANKRUPTCY))
+}
+error.cv <- function (formula, data, thresh, seed.num) {
+    data.len = length(data[,1])
+    set.seed(seed.num)
+    train = sample(1:data.len, ceiling(data.len/2))
+    train.data = data[train,]
+    test.data = data[-train,]
 
-regfit.full <- subset.simple(data, 'full')
+    return(error.glm(formula, train.data, test.data, 0.5))
+}
+error.loocv <- function (formula, data) {
+}
+cross.validation <- function (regfit, data, seed.num) {
+    
+    cp = summary(regfit)$cp
+    bic = summary(regfit)$bic
+    adjr2 = summary(regfit)$adjr2
+
+    coef.cp = coef(regfit, which.min(cp))
+    coef.bic = coef(regfit, which.min(bic))
+    coef.adjr2 = coef(regfit, which.max(adjr2))
+
+    ## vars.cp = clear.names(names(coef.cp))
+    ## vars.bic = clear.names(names(coef.bic))
+    ## vars.adjr2 = clear.names(names(coef.adjr2))
+
+    ## formula.cp = gen.formula("BANKRUPTCY", vars.cp)
+    ## formula.bic = gen.formula("BANKRUPTCY", vars.bic)
+    ## formula.adjr2 = gen.formula("BANKRUPTCY", vars.adjr2)
+    formula.cp = gen.formula(coef.cp, "BANKRUPTCY")
+    formula.bic = gen.formula(coef.bic, "BANKRUPTCY")
+    formula.adjr2 = gen.formula(coef.adjr2, "BANKRUPTCY")
+
+    print(error.cv(formula.cp, data, 0.5, seed.num))
+    ## set.seed(seed.num)
+    ## train = sample(1:data.len, floor(data.len/2))
+    ## train.data = data[train,]
+    ## test.data = data[-train,]
+    
+    ## glmfit.cp = glm(formula.cp, data=train.data, family="binomial")
+    ## glmfit.bic = glm(formula.bic, data=train.data, family="binomial")
+    ## glmfit.adjr2 = glm(formula.adjr2, data=train.data, family="binomial")
+
+    ## res.cp = predict(glmfit.cp, data=test.data, type="response")
+    ## res.bic = predict(glmfit.bic, data=test.data, type="response")
+    ## res.adjr2 = predict(glmfit.adjr2, data=test.data, type="response")
+    ## if (any(res.cp < 0 | res.cp > 1)) stop ("예측한 Cp값이 0 또는 1이 아닙니다.")
+    ## if (any(res.bic < 0 | res.bic > 1)) stop ("예측한 BIC값이 0 또는 1이 아닙니다.")
+    ## if (any(res.adjr2 < 0 | res.adjr2 > 1)) stop ("예측한 Adjr2값이 0 또는 1이 아닙니다.")
+}
+
+
+regfit.full <- subset.simple(data)
 regfit.fwd <- subset.simple(data, 'forward')
-regfix.bwd <- subset.simple(data, 'backward')
-plot.gen(regfit.fwd, './plots/forward.png')
-plot.gen(regfit.bwd, './plots/backward.png')
-## print(summary(regfit.full))
+regfit.bwd <- subset.simple(data, 'backward')
 
+plot.gen(regfit.full, './plots/tmp/full.png')
+plot.gen(regfit.fwd, './plots/tmp/forward.png')
+plot.gen(regfit.bwd, './plots/tmp/backward.png')
 
 coef.print(regfit.full)
 coef.print(regfit.fwd)
 coef.print(regfit.bwd)
+
+predict.regsubsets(coef(regfit.fwd, which.min(summary(regfit.fwd)$cp)), data)
